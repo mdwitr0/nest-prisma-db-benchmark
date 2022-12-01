21fafd0e895ffba801f9dc3b866a7338ee2bb5c1
mdwitr0 <mdwit0r@gmail.com>
2022-12-01 23:37:46 +0100

    add run app logging

--- apps/mongodb/src/main.ts
+++ apps/mongodb/src/main.ts
@@ -4,12 +4,16 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
   await app.listen(3111);
+
+  logger.log('🔥 Mongodb application is running on: ' + (await app.getUrl()));
 }
 bootstrap();
--- apps/postgresql/src/main.ts
+++ apps/postgresql/src/main.ts
@@ -4,12 +4,19 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
   await app.listen(3112);
+
+  logger.log(
+    '🔥 Postgresql application is running on: ' + (await app.getUrl())
+  );
 }
 bootstrap();
