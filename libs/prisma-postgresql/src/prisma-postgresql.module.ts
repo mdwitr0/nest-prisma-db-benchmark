65706f81adb2e21cb5577bed8b970864ab77f1f6
mdwitr0 <mdwit0r@gmail.com>
2022-12-11 00:28:07 +0100

    fear(postgresql): add middleware

--- libs/prisma-postgresql/src/prisma-postgresql.module.ts
+++ libs/prisma-postgresql/src/prisma-postgresql.module.ts
@@ -1,9 +1,74 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import {
+  PrismaModuleAsyncOptions,
+  PrismaModuleOptions,
+  PrismaOptionsFactory,
+} from './prisma-postgresql-module-options.interface';
+import { PRISMA_SERVICE_OPTIONS } from './prisma-postgresql.constants';
 import { PrismaPostgresqlService } from './prisma-postgresql.service';
 
 @Module({
-  controllers: [],
   providers: [PrismaPostgresqlService],
   exports: [PrismaPostgresqlService],
 })
-export class PrismaPostgresqlModule {}
+export class PrismaPostgresqlModule {
+  static forRoot(options: PrismaModuleOptions = {}): DynamicModule {
+    return {
+      global: options.isGlobal,
+      module: PrismaPostgresqlModule,
+      providers: [
+        {
+          provide: PRISMA_SERVICE_OPTIONS,
+          useValue: options.prismaServiceOptions,
+        },
+      ],
+    };
+  }
+
+  static forRootAsync(options: PrismaModuleAsyncOptions): DynamicModule {
+    return {
+      global: options.isGlobal,
+      module: PrismaPostgresqlModule,
+      imports: options.imports || [],
+      providers: this.createAsyncProviders(options),
+    };
+  }
+
+  private static createAsyncProviders(
+    options: PrismaModuleAsyncOptions
+  ): Provider[] {
+    if (options.useExisting || options.useFactory) {
+      return this.createAsyncOptionsProvider(options);
+    }
+
+    return [
+      ...this.createAsyncOptionsProvider(options),
+      {
+        provide: options.useClass,
+        useClass: options.useClass,
+      },
+    ];
+  }
+
+  private static createAsyncOptionsProvider(
+    options: PrismaModuleAsyncOptions
+  ): Provider[] {
+    if (options.useFactory) {
+      return [
+        {
+          provide: PRISMA_SERVICE_OPTIONS,
+          useFactory: options.useFactory,
+          inject: options.inject || [],
+        },
+      ];
+    }
+    return [
+      {
+        provide: PRISMA_SERVICE_OPTIONS,
+        useFactory: async (optionsFactory: PrismaOptionsFactory) =>
+          await optionsFactory.createPrismaOptions(),
+        inject: [options.useExisting || options.useClass],
+      },
+    ];
+  }
+}
--- libs/prisma-postgresql/src/prisma-postgresql.service.ts
+++ libs/prisma-postgresql/src/prisma-postgresql.service.ts
@@ -1,16 +1,46 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from './generated/client';
+import {
+  INestApplication,
+  INestMicroservice,
+  Inject,
+  Injectable,
+  OnModuleInit,
+  Optional,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from './generated/client';
+import { PrismaServiceOptions } from './prisma-postgresql-module-options.interface';
+import { PRISMA_SERVICE_OPTIONS } from './prisma-postgresql.constants';
 
 @Injectable()
 export class PrismaPostgresqlService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
+  extends PrismaClient<
+    Prisma.PrismaClientOptions,
+    'query' | 'info' | 'warn' | 'error'
+  >
+  implements OnModuleInit
 {
-  async onModuleInit() {
-    await this.$connect();
+  constructor(
+    @Optional()
+    @Inject(PRISMA_SERVICE_OPTIONS)
+    private readonly prismaServiceOptions: PrismaServiceOptions = {}
+  ) {
+    super(prismaServiceOptions.prismaOptions);
+
+    if (this.prismaServiceOptions.middlewares) {
+      this.prismaServiceOptions.middlewares.forEach((middleware) =>
+        this.$use(middleware)
+      );
+    }
   }
 
-  async onModuleDestroy() {
-    await this.$disconnect();
+  async onModuleInit() {
+    if (this.prismaServiceOptions.explicitConnect) {
+      await this.$connect();
+    }
+  }
+
+  async enableShutdownHooks(app: INestApplication | INestMicroservice) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
   }
 }
