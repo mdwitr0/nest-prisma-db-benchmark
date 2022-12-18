0066e9c6832dd9688fb640523713f9b217a227e7
mdwitr0 <mdwit0r@gmail.com>
2022-12-18 11:08:16 +0100

    chore(tracing): remove jaerger

--- libs/tracing/src/lib/tracing.init.ts
+++ libs/tracing/src/lib/tracing.init.ts
@@ -1,33 +1,15 @@
 import { Logger } from '@nestjs/common';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
-import {
-  CompositePropagator,
-  W3CBaggagePropagator,
-  W3CTraceContextPropagator,
-} from '@opentelemetry/core';
-import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import { B3InjectEncoding, B3Propagator } from '@opentelemetry/propagator-b3';
-import { JaegerPropagator } from '@opentelemetry/propagator-jaeger';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 import { Resource } from '@opentelemetry/resources';
 import * as opentelemetry from '@opentelemetry/sdk-node';
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { PrismaInstrumentation } from '@prisma/instrumentation';
 import * as process from 'process';
-import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
-import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 
 export const initTracing = async (
   serviceName: string,
   metricsPort: number
 ): Promise<void> => {
   const logger = new Logger('Tracing');
-  const traceExporter = new JaegerExporter();
-
-  const spanProcessor = new SimpleSpanProcessor(traceExporter);
 
   const sdk = new opentelemetry.NodeSDK({
     metricReader: new PrometheusExporter({
@@ -36,28 +18,6 @@ export const initTracing = async (
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
     }),
-    contextManager: new AsyncLocalStorageContextManager(),
-    textMapPropagator: new CompositePropagator({
-      propagators: [
-        new JaegerPropagator(),
-        new W3CTraceContextPropagator(),
-        new W3CBaggagePropagator(),
-        new B3Propagator(),
-        new B3Propagator({
-          injectEncoding: B3InjectEncoding.MULTI_HEADER,
-        }),
-      ],
-    }),
-    instrumentations: [
-      getNodeAutoInstrumentations(),
-      new ExpressInstrumentation(),
-      new NestInstrumentation(),
-      new HttpInstrumentation(),
-      new PrismaInstrumentation(),
-    ],
-    // Using a simple span processor for faster response.
-    // You can also use the batch processor instead.
-    spanProcessor,
   });
 
   try {
